44dac973029da671a603ee568f23f81595a492d4
Breno Garcia <BrenoGHeat@gmail.com>
2024-06-14 14:52:24 -0300

    Componentization started

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,27 +4,27 @@ import { MdSearch, MdShoppingCart } from "react-icons/md";
 import styles from "./style.module.scss";
 
 export const Header = () => {
-   const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-   return (
-      <header className={styles.header}>
-         <img src={Logo} alt="Logo Kenzie Burguer" />
-         <div>
-            <button>
-                <MdShoppingCart size={21} />
-                <span>0</span>
-            </button>
-            <form>
-               <input
-                  type="text"
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-               />
-               <button type="submit">
-                 <MdSearch size={21} />
-               </button>
-            </form>
-         </div>
-      </header>
-   );
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="Logo Kenzie Burguer" />
+      <div>
+        <button>
+          <MdShoppingCart size={21} />
+          <span>0</span>
+        </button>
+        <form>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">
+            <MdSearch size={21} />
+          </button>
+        </form>
+      </div>
+    </header>
+  );
 };
